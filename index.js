0402a7d63b4dc283ddf835819a44067e2adeb91c
kononenkoAnton <a.kononenko@applicaster.com>
2020-05-07 15:53:41 -0400

    Bump version 0.0.4

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ export const JWPlayerState =
 
 export default class JWPlayer extends Component {
   static propTypes = {
-    licenseKey: PropTypes.string
+    licenseKey: PropTypes.string,
     file: PropTypes.string,
     image: PropTypes.string,
     title: PropTypes.string,
